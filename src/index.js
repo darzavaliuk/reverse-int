9a226034b823a22d506b0c032351d7f7a3708b31
Daria <dar.zavaliuk@gmail.com>
2022-07-05 00:03:00 +0300

    Result

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 module.exports = function reverse (n) {
     n = n + "";
-    return n.split("").reverse("").join("");
+    if (n > 0) {
+        return n.split("").reverse("").join("")
+    }
+    else {
+        let res = Math.abs(n).toString();
+        return res.split("").reverse("").join("")
+    }
+        
 }
